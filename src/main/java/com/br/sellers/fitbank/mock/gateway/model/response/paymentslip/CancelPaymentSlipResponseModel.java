1106ec0c7cc1c7ff88e246c13cc76bb2becdfa2c
Marcelo Vieira <marcelosouzav@gmail.com>
2022-10-24 10:09:18 -0300

    Add cerviço para cancelar boleto

--- src/main/java/com/br/sellers/fitbank/mock/gateway/model/response/paymentslip/CancelPaymentSlipResponseModel.java
+++ src/main/java/com/br/sellers/fitbank/mock/gateway/model/response/paymentslip/CancelPaymentSlipResponseModel.java
@@ -1,18 +1,12 @@
 package com.br.sellers.fitbank.mock.gateway.model.response.paymentslip;
 
 import com.br.sellers.fitbank.mock.gateway.model.response.BasicResponseModel;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.google.gson.annotations.SerializedName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class CancelPaymentSlipResponseModel extends BasicResponseModel {
 
 
