6fe2cd4e7f658f08f8a4d905c82040a39de229fe
paulamachaddo <paularenatams@gmail.com>
2023-01-19 19:23:32 -0300

    Criando regras de campos obrigatórios no GenerateBoleto.

--- src/main/java/com/br/sellers/fitbank/mock/service/paymentslip/CreatePaymentSlipService.java
+++ src/main/java/com/br/sellers/fitbank/mock/service/paymentslip/CreatePaymentSlipService.java
@@ -7,6 +7,7 @@ import com.br.sellers.fitbank.mock.gateway.model.response.paymentslip.GetPayment
 import com.br.sellers.fitbank.mock.service.ServiceBaseImpl;
 import com.google.gson.Gson;
 
+import java.util.Map;
 import java.util.Random;
 
 public class CreatePaymentSlipService extends ServiceBaseImpl {
@@ -30,4 +31,32 @@ public class CreatePaymentSlipService extends ServiceBaseImpl {
         modelResponde.setAlreadyExists(false);
     }
 
+    @Override
+    protected void validadeFields(BasicRequestModel request, Map<String, Object> errors) {
+        CreatePaymentSlipRequestModel model = (CreatePaymentSlipRequestModel) request;
+
+        validadeField(model.getGroupTemplate(), "GroupTemplate", "GroupTemplate é obrigatório", errors);
+        validadeField(model.getCustomerName(), "CustomerName", "CustomerName é obrigatório", errors);
+        validadeField(model.getCustomerTaxNumber(), "CustomerTaxNumber", "CustomerTaxNumber é obrigatório", errors);
+        validadeField(model.getCustomerMail(), "CustomerMail", "CustomerMail é obrigatório", errors);
+        validadeField(model.getCustomerPhone(), "CustomerPhone", "CustomerPhone é obrigatório", errors);
+        validadeField(model.getNeighborhood(), "Neighborhood", "Neighborhood é obrigatório", errors);
+        validadeField(model.getCity(), "City", "City é obrigatório", errors);
+        validadeField(model.getState(), "State", "State é obrigatório", errors);
+        validadeField(model.getZipCode(), "ZipCode", "ZipCode é obrigatório", errors);
+        validadeField(model.getSupplierTaxNumber(), "SupplierTaxNumber", "SupplierTaxNumber é obrigatório", errors);
+        validadeField(model.getSupplierFullName(), "SupplierFullName", "SupplierFullName é obrigatório", errors);
+        validadeField(model.getSupplierTradingName(), "SupplierTradingName", "SupplierTradingName é obrigatório", errors);
+        validadeField(model.getSupplierLegalName(), "SupplierLegalName", "SupplierLegalName é obrigatório", errors);
+        validadeField(model.getSupplierMail(), "SupplierMail", "SupplierMail é obrigatório", errors);
+        validadeField(model.getSupplierPhone(), "SupplierPhone", "SupplierPhone é obrigatório", errors);
+        validadeField(model.getRateValue(), "RateValue", "RateValue é obrigatório", errors);
+        validadeField(model.getRateSent    (), "RateSent", "RateSent é obrigatório", errors);
+        validadeField(model.getAddressLine1(), "AddressLine1", "AddressLine1 é obrigatório", errors);
+        validadeField(model.getExternalNumber(), "ExternalNumber", "ExternalNumber é obrigatório", errors);
+        validadeField(model.getIdentifier(), "Identifier", "Identifier é obrigatório", errors);
+        validadeField(model.getProducts(), "Products", "Products é obrigatório", errors);
+        validadeField(model.getDueDate(), "DueDate", "DueDate é obrigatório", errors);
+        validadeField(model.getTotalValue(), "TotalValue", "TotalValue é obrigatório", errors);
+    }
 }
